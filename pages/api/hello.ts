3f64bbaa16b5fa450001f3c2d1a91ab08c840e81
pensuke <pensuke81@gmail.com>
2022-10-28 09:20:05 +0900

    experimental flag

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,16 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+export const config = {
+  runtime: "experimental-edge",
+};
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler() {
+  return new Response(JSON.stringify({ name: "John Doe" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
